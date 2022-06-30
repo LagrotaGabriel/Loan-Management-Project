85516c490052aa58b42e2d5264bbcef8878cffc1
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-30 17:11:33 -0300

    -> I added the componentscans, covering all project structure

--- backend/loan-api/src/main/java/br/com/loanapi/LoanApiApplication.java
+++ backend/loan-api/src/main/java/br/com/loanapi/LoanApiApplication.java
@@ -2,9 +2,17 @@ package br.com.loanapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EntityScan(basePackages = {"br.com.loanapi.models"})
+@EnableJpaRepositories("br.com.loanapi.repositories")
+@ComponentScan("br.com.loanapi.services")
+@ComponentScan("br.com.loanapi.resources")
+@ComponentScan("br.com.loanapi.config")
+@ComponentScan("br.com.loanapi.exceptions")
+@SpringBootApplication
 public class LoanApiApplication {
 
 	public static void main(String[] args) {
