d89e8bdf54ac46ce82a9c7ee3bb57f5ed803f9c7
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-01 17:06:26 -0300

    I Built the tests of main class.

--- backend/loan-api/src/test/java/br/com/loanapi/LoanApiApplicationTests.java
+++ backend/loan-api/src/test/java/br/com/loanapi/LoanApiApplicationTests.java
@@ -1,13 +1,19 @@
 package br.com.loanapi;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DisplayName("Main class test")
 class LoanApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		LoanApiApplication.main(new String[] {});
+		boolean ok = true;
+		Assertions.assertTrue(ok);
 	}
 
 }
