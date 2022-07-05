c37da6abcc75ff44bfb57e1c7781585c79ac35d9
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-05 08:29:06 -0300

    I changed the swagger configurations

--- backend/loan-api/src/main/java/br/com/loanapi/config/SwaggerConfig.java
+++ backend/loan-api/src/main/java/br/com/loanapi/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,7 +27,7 @@ public class SwaggerConfig {
     public Docket productApi(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.frases"))
+                .apis(RequestHandlerSelectors.basePackage("br.com.loanapi"))
                 .paths(regex("/api.*"))
                 .build()
                 .apiInfo(metaInfo());
@@ -36,16 +35,16 @@ public class SwaggerConfig {
 
     private ApiInfo metaInfo(){
         return new ApiInfo(
-                "Frases",
-                "API Rest para recebimento e envio de frases",
+                "Loan project - CRUD",
+                "Api rest that aims to establish the loan project crud",
                 "1.0.0",
                 "Terms of Service",
                 new Contact("Gabriel Lagrota",
-                        "https://github.com/LagrotaGabriel/API-Frases",
+                        "https://github.com/LagrotaGabriel/Loan-Project",
                         "gabriellagrota23@gmail.com"),
                 "Apache License Version 2.0",
                 "https://www.apache.org/licensen.html",
-                new ArrayList<VendorExtension>()
+                new ArrayList<>()
         );
     }
 
