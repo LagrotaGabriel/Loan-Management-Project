4c1fda0ca88bf59e16dcf8476374f66b51134a66
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-06 17:40:50 -0300

    I built a class to project Regex

---
+++ backend/loan-api/src/main/java/br/com/loanapi/utils/RegexPatterns.java
@@ -0,0 +1,19 @@
+package br.com.loanapi.utils;
+
+public class RegexPatterns {
+
+    RegexPatterns(){}
+
+    public static final String STREET_NUMBER_REGEX_PATTERN = "[0-9]{1,5}";
+
+    public static final String POSTAL_CODE_REGEX_PATTERN = "[0-9]{5}-[0-9]{3}";
+
+    public static final String RG_REGEX_PATTERN = "[0-9]{2}.[0-9]{3}.[0-9]{3}-[0-9]";
+
+    public static final String CPF_REGEX_PATTERN = "[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}";
+
+    public static final String EMAIL_REGEX_PATTERN =
+            "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+
+
+}
