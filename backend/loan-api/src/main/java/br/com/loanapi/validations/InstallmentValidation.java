6559d54e3d705b80f66b68c4a4be947768e36e0a
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-08 10:23:03 -0300

    I built the Installment validation class and his tests with 100% coverage

---
+++ backend/loan-api/src/main/java/br/com/loanapi/validations/InstallmentValidation.java
@@ -0,0 +1,49 @@
+package br.com.loanapi.validations;
+
+import br.com.loanapi.exceptions.InvalidRequestException;
+import br.com.loanapi.models.dto.InstallmentDTO;
+
+import static br.com.loanapi.utils.RegexPatterns.INSTALLMENT_MONTH_NUMBER;
+
+public class InstallmentValidation {
+
+    public boolean validateRequest(InstallmentDTO installment) {
+        verifyMonth(installment.getMonth());
+        verifyAmortization(installment.getAmortization());
+        verifyInterest(installment.getInterest());
+        verifyValue(installment.getValue());
+        return true;
+    }
+
+    public boolean notNull(InstallmentDTO installment) {
+        if(installment.getMaturityDate() != null &&
+        installment.getExpired() != null &&
+        installment.getMonth() != null &&
+        installment.getAmortization() != null &&
+        installment.getInterest() != null &&
+        installment.getValue() != null &&
+        installment.getLoan() != null) return true;
+        throw new InvalidRequestException("Installment validation failed. Some of the attributes is null or empty");
+    }
+
+    public boolean verifyMonth(Integer month){
+        if(month.toString().matches(INSTALLMENT_MONTH_NUMBER)) return true;
+        throw new InvalidRequestException("Installment validation failed. The month number may be between 1-999");
+    }
+
+    public boolean verifyAmortization(Double amortization){
+        if (amortization > 0.0 && amortization < 999999999.0) return true;
+        throw new InvalidRequestException("Amortization validation failed");
+    }
+
+    public boolean verifyInterest(Double interest){
+        if (interest > 0.0 && interest < 999999999.0) return true;
+        throw new InvalidRequestException("Interest validation failed");
+    }
+
+    public boolean verifyValue(Double value){
+        if (value > 0.0 && value < 999999999.0) return true;
+        throw new InvalidRequestException("Value validation failed");
+    }
+
+}
---
+++ backend/loan-api/src/test/java/br/com/loanapi/validations/InstallmentValidationTest.java
@@ -0,0 +1,131 @@
+package br.com.loanapi.validations;
+
+import br.com.loanapi.exceptions.InvalidRequestException;
+import br.com.loanapi.mocks.dto.InstallmentDTODataBuilder;
+import br.com.loanapi.models.dto.InstallmentDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.ParseException;
+
+@SpringBootTest(classes = InstallmentValidation.class)
+@DisplayName("Validation: Installment")
+class InstallmentValidationTest {
+
+    @InjectMocks
+    InstallmentValidation validation;
+
+    @Test
+    @DisplayName("Should validate month validation with success")
+    void shouldValidateMonthValidationWithSuccess() throws ParseException {
+        Assertions.assertTrue(validation.verifyMonth(InstallmentDTODataBuilder.builder().build().getMonth()));
+    }
+
+    @Test
+    @DisplayName("Should validate month validation with exception")
+    void shouldValidateMonthValidationWithException(){
+
+        try{
+            validation.verifyMonth(InstallmentDTODataBuilder.builder().withTooLongMonth().build().getMonth());
+            Assertions.fail();
+        }
+        catch(InvalidRequestException | ParseException exception){
+            Assertions.assertEquals("Installment validation failed. The month number may be between 1-999",
+                    exception.getMessage());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Should validate amortization validation with success")
+    void shouldValidateAmortizationValidationWithSuccess() throws ParseException {
+        Assertions.assertTrue(validation.verifyAmortization(InstallmentDTODataBuilder.builder().build().getAmortization()));
+    }
+
+    @Test
+    @DisplayName("Should validate amortization validation with exception")
+    void shouldValidateAmortizationValidationWithException(){
+
+        try{
+            validation.verifyAmortization(InstallmentDTODataBuilder.builder().withTooLongAmortization().build().getAmortization());
+            Assertions.fail();
+        }
+        catch(InvalidRequestException | ParseException exception){
+            Assertions.assertEquals("Amortization validation failed", exception.getMessage());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Should validate interest validation with success")
+    void shouldValidateInterestValidationWithSuccess() throws ParseException {
+        Assertions.assertTrue(validation.verifyInterest(InstallmentDTODataBuilder.builder().build().getInterest()));
+    }
+
+    @Test
+    @DisplayName("Should validate interest validation with exception")
+    void shouldValidateInterestValidationWithException(){
+
+        try{
+            validation.verifyInterest(InstallmentDTODataBuilder.builder().withTooLongInterest().build().getInterest());
+            Assertions.fail();
+        }
+        catch(InvalidRequestException | ParseException exception){
+            Assertions.assertEquals("Interest validation failed", exception.getMessage());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Should validate value validation with success")
+    void shouldValidateValueValidationWithSuccess() throws ParseException {
+        Assertions.assertTrue(validation.verifyValue(InstallmentDTODataBuilder.builder().build().getValue()));
+    }
+
+    @Test
+    @DisplayName("Should validate Value validation with exception")
+    void shouldValidateValueValidationWithException(){
+
+        try{
+            validation.verifyValue(InstallmentDTODataBuilder.builder().withTooLongValue().build().getValue());
+            Assertions.fail();
+        }
+        catch(InvalidRequestException | ParseException exception){
+            Assertions.assertEquals("Value validation failed", exception.getMessage());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Should validate not null validation with success")
+    void shouldValidateNotNullValidationWithSuccess() throws ParseException {
+        Assertions.assertTrue(validation.notNull(InstallmentDTODataBuilder.builder().build()));
+    }
+
+    @Test
+    @DisplayName("Should validate not null validation with exception")
+    void shouldValidateNotNullValidationWithException() throws ParseException {
+        InstallmentDTO installment = InstallmentDTODataBuilder.builder().build();
+        installment.setLoan(null);
+
+        try{
+            validation.notNull(installment);
+            Assertions.fail();
+        }
+        catch(InvalidRequestException exception){
+            Assertions.assertEquals("Installment validation failed. Some of the attributes is null or empty",
+                    exception.getMessage());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Should validate validate request with success")
+    void shouldValidateValidateRequestWithSuccess() throws ParseException {
+        Assertions.assertTrue(validation.validateRequest(InstallmentDTODataBuilder.builder().build()));
+    }
+
+}
