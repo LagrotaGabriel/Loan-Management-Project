68e26b3f038383629ff673e2b5e7b95188c7ed21
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-08 10:25:29 -0300

    I added the month number regex pattern

--- backend/loan-api/src/main/java/br/com/loanapi/utils/RegexPatterns.java
+++ backend/loan-api/src/main/java/br/com/loanapi/utils/RegexPatterns.java
@@ -4,6 +4,8 @@ public class RegexPatterns {
 
     RegexPatterns(){}
 
+    public static final String INSTALLMENT_MONTH_NUMBER = "[1-9]{1,3}";
+
     public static final String STREET_NUMBER_REGEX_PATTERN = "[0-9]{1,5}";
 
     public static final String POSTAL_CODE_REGEX_PATTERN = "[0-9]{5}-[0-9]{3}";
