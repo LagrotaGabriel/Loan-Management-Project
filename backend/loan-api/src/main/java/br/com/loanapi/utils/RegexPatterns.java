d6b2fb5d9ee63b51e60ec2e4604fe635633a6201
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-12 09:38:04 -0300

    I added some REGEX PATTERNS into RegexPatterns util class

--- backend/loan-api/src/main/java/br/com/loanapi/utils/RegexPatterns.java
+++ backend/loan-api/src/main/java/br/com/loanapi/utils/RegexPatterns.java
@@ -4,6 +4,8 @@ public class RegexPatterns {
 
     RegexPatterns(){}
 
+    public static final String SCORE_PATTERN = "[0-9]{1,3}.[0-9]{1,2}";
+
     public static final String INSTALLMENT_MONTH_NUMBER = "[0-9]{1,3}";
 
     public static final String STREET_NUMBER_REGEX_PATTERN = "[0-9]{1,5}";
@@ -17,5 +19,8 @@ public class RegexPatterns {
     public static final String EMAIL_REGEX_PATTERN =
             "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
 
+    public static final String PHONE_REGEX_PATTERN = "[1-9][0-9]{3,4}-[0-9]{4}";
+
+    public static final String PHONE_PREFIX_REGEX_PATTERN = "[1-9]{2}";
 
 }
