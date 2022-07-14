d901c593a48e7bae6aa80ad313e255c0ab5f3f0a
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-14 11:29:03 -0300

    I created the creation service method and logged his lines.

--- backend/loan-api/src/main/java/br/com/loanapi/services/AddressService.java
+++ backend/loan-api/src/main/java/br/com/loanapi/services/AddressService.java
@@ -4,23 +4,39 @@ import br.com.loanapi.config.ModelMapperConfig;
 import br.com.loanapi.exceptions.InvalidRequestException;
 import br.com.loanapi.exceptions.ObjectNotFoundException;
 import br.com.loanapi.models.dto.AddressDTO;
+import br.com.loanapi.models.dto.CityDTO;
 import br.com.loanapi.models.entities.AddressEntity;
+import br.com.loanapi.models.entities.CityEntity;
 import br.com.loanapi.repositories.AddressRepository;
+import br.com.loanapi.repositories.CityRepository;
 import br.com.loanapi.validations.AddressValidation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class AddressService {
 
     @Autowired
     AddressRepository repository;
 
+    @Autowired
+    CityRepository cityRepository;
+
+    @Autowired
+    CityService cityService;
+
     String ADDRESS_NOT_FOUND = "Address not found";
+
+    String VALIDATION_FAILED = "Address validation failed";
+
+    String LOG_BAR = "===========================================================================================";
 
     @Autowired
     ModelMapperConfig modelMapper;
@@ -28,10 +44,79 @@ public class AddressService {
     AddressValidation validation = new AddressValidation();
 
     public AddressDTO create(AddressDTO address){
-        if(validation.validateRequest(address))
-            return modelMapper.mapper().map(
-                    repository.save(modelMapper.mapper().map(address, AddressEntity.class)), AddressDTO.class);
-        throw new InvalidRequestException("Address validation failed");
+
+        log.info(LOG_BAR);
+        log.info("[STARTING] Starting service create method");
+
+        if(validation.validateRequest(address)) {
+
+            log.info("[PROGRESS] Searching for a CityEntity with the city name: {} and state: {}", address.getCity().getCity(), address.getCity().getState().getPrefix());
+            Optional<CityEntity> city = cityRepository.findByCityAndState(address.getCity().getCity(), address.getCity().getState());
+
+            if (city.isPresent()) {
+
+                log.warn("[INFO] City finded: {}", city.get().getCity() + " - " + city.get().getState().getPrefix());
+                CityDTO cityDTO = modelMapper.mapper().map(city.get(), CityDTO.class);
+                address.setCity(cityDTO);
+
+                log.info("[PROGRESS] Checking if the current city have addresses saved...");
+                if (cityDTO.getAddresses() == null){
+
+                    log.warn("[INFO] The CityEntity addresses list is null");
+
+                    log.info("[PROGRESS] Creating a null arraylist for the CityEntity adresses...");
+                    List<AddressDTO> addresses = new ArrayList<>();
+
+                    log.info("[PROGRESS] Adding the address to ArrayList...");
+                    addresses.add(address);
+
+                    log.info("[PROGRESS] Setting the CityDTO addresses list with the new ArrayList now instantiated...");
+                    cityDTO.setAddresses(addresses);
+
+                }
+                else {
+
+                    log.warn("[INFO] The CityEntity addresses list is filled");
+
+                    log.info("[PROGRESS] Adding the address to CityEntity addresses list...");
+                    cityDTO.getAddresses().add(address);
+
+                }
+
+                log.info("[PROGRESS] Updating the city with a new address in database...");
+                cityRepository.save(modelMapper.mapper().map(cityDTO, CityEntity.class));
+
+            }
+
+            else{
+
+                log.warn("[PROGRESS] City not found. Creating a new city...");
+
+                CityDTO cityDTO = address.getCity();
+                log.info("[PROGRESS] Instantiated a variable cityDTO with City value included... {}", cityDTO);
+
+                log.info("[PROGRESS] Creating a null arraylist for the CityEntity adresses...");
+                List<AddressDTO> addresses = new ArrayList<>();
+
+                log.info("[PROGRESS] Adding the address {} to addresses ArrayList...", address);
+                addresses.add(address);
+
+                log.info("[PROGRESS] Setting the CityDTO addresses list with the new ArrayList now instantiated...");
+                cityDTO.setAddresses(addresses);
+
+                log.info("[PROGRESS] CityEntity and AddressEntity are being created at database...");
+                cityRepository.save(modelMapper.mapper().map(cityDTO, CityEntity.class));
+
+            }
+
+            log.info("[SUCCESS] Request successfull");
+            return address;
+
+        }
+
+        log.error("[FAILURE] " + VALIDATION_FAILED);
+        throw new InvalidRequestException(VALIDATION_FAILED);
+
     }
 
     public List<AddressDTO> findAll(){
@@ -63,7 +148,7 @@ public class AddressService {
                     modelMapper.mapper().map(dto, AddressEntity.class)), AddressDTO.class);
         }
         else{
-            throw new InvalidRequestException("Address validation failed");
+            throw new InvalidRequestException(VALIDATION_FAILED);
         }
 
     }
