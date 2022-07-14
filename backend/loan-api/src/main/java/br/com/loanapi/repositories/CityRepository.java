27e8ce6f2d54748ab37700703e2b75fa1962c7c8
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-14 11:23:07 -0300

    I added a @Query that finds a City by city name and state

--- backend/loan-api/src/main/java/br/com/loanapi/repositories/CityRepository.java
+++ backend/loan-api/src/main/java/br/com/loanapi/repositories/CityRepository.java
@@ -1,8 +1,17 @@
 package br.com.loanapi.repositories;
 
 import br.com.loanapi.models.entities.CityEntity;
+import br.com.loanapi.models.enums.StateEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CityRepository extends JpaRepository<CityEntity, Long> {}
+public interface CityRepository extends JpaRepository<CityEntity, Long> {
+
+    @Query("Select c From CityEntity c where c.city = ?1 and c.state = ?2")
+    Optional<CityEntity> findByCityAndState(String city, StateEnum state);
+
+}
