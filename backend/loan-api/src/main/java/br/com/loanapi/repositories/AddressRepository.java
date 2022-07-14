361229e3dd8ed3d963f7cf3a192d29e6b446f57a
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-14 11:23:40 -0300

    I added a @Query that finds a City by city street, number and postal code

--- backend/loan-api/src/main/java/br/com/loanapi/repositories/AddressRepository.java
+++ backend/loan-api/src/main/java/br/com/loanapi/repositories/AddressRepository.java
@@ -2,7 +2,15 @@ package br.com.loanapi.repositories;
 
 import br.com.loanapi.models.entities.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AddressRepository extends JpaRepository<AddressEntity, Long> {}
+public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
+
+    @Query("Select a From AddressEntity a where a.street = ?1 and a.number = ?2 and a.postalCode = ?3")
+    Optional<AddressEntity> findByStreetNumberAndPostalCode(String street, Integer number, String postalCode);
+
+}
