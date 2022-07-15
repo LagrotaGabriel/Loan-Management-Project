3d10a90ce08c21925c9c9415baf873f4d0f7f1ab
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-15 15:02:07 -0300

    I built some query searches at CustomerRepository

--- backend/loan-api/src/main/java/br/com/loanapi/repositories/CustomerRepository.java
+++ backend/loan-api/src/main/java/br/com/loanapi/repositories/CustomerRepository.java
@@ -2,7 +2,21 @@ package br.com.loanapi.repositories;
 
 import br.com.loanapi.models.entities.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {}
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+
+    @Query("Select c From CustomerEntity c where c.rg = ?1")
+    Optional<CustomerEntity> findByRg(String rg);
+
+    @Query("Select c From CustomerEntity c where c.cpf = ?1")
+    Optional<CustomerEntity> findByCpf(String cpf);
+
+    @Query("Select c From CustomerEntity c where c.email = ?1")
+    Optional<CustomerEntity> findByEmail(String email);
+
+}
