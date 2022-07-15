657e664962af8b0a37242e14cad48a8b26f97c11
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-15 11:01:04 -0300

    Constant util class created to make a pattern to logger strings

---
+++ backend/loan-api/src/main/java/br/com/loanapi/utils/StringConstants.java
@@ -0,0 +1,20 @@
+package br.com.loanapi.utils;
+
+public class StringConstants {
+
+    StringConstants(){}
+
+    public static final String ADDRESS_NOT_FOUND = "Address not found";
+
+    public static final String ADDRESS_VALIDATION_FAILED = "Address validation failed";
+
+    public static final String CITY_VALIDATION_FAILED = "City validation failed";
+
+   public static final String CITY_NOT_FOUND = "City not found";
+
+    public static final String LOG_BAR = "=======================================================================================" +
+            "==========================================";
+
+    public static final String REQUEST_SUCCESSFULL = "[SUCCESS] Request successfull";
+
+}
