34b78d1c8965487e7e3ef9b62e28f7ff83db9c70
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-20 16:25:15 -0300

    I built a query search at PhoneRepository to find a Phone by Prefix and Number

--- backend/loan-api/src/main/java/br/com/loanapi/repositories/PhoneRepository.java
+++ backend/loan-api/src/main/java/br/com/loanapi/repositories/PhoneRepository.java
@@ -2,7 +2,15 @@ package br.com.loanapi.repositories;
 
 import br.com.loanapi.models.entities.PhoneEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface PhoneRepository extends JpaRepository<PhoneEntity, Long> {}
+public interface PhoneRepository extends JpaRepository<PhoneEntity, Long> {
+
+    @Query("Select p From Phone p Where p.prefix=?1 and p.number=?2")
+    Optional<PhoneEntity> findByPrefixAndNumber(Integer prefix, String number);
+
+}
