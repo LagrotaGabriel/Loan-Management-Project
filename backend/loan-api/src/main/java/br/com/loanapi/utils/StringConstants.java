a857432c5d82aac4d7bedb4e58b238f4a7839145
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-27 15:51:31 -0300

    New String constants added to his util class

--- backend/loan-api/src/main/java/br/com/loanapi/utils/StringConstants.java
+++ backend/loan-api/src/main/java/br/com/loanapi/utils/StringConstants.java
@@ -5,12 +5,15 @@ public class StringConstants {
     StringConstants(){}
 
     public static final String ADDRESS_NOT_FOUND_LOG = "[FAILURE]  Address not found";
+    public static final String CUSTOMER_NOT_FOUND_LOG = "[FAILURE]  Customer not found";
 
     public static final String ADDRESS_NOT_FOUND = "Address not found";
 
     public static final String ADDRESS_VALIDATION_FAILED_LOG = "[FAILURE]  Address validation failed";
+    public static final String CUSTOMER_VALIDATION_FAILED_LOG = "[FAILURE]  Customer validation failed";
 
     public static final String ADDRESS_VALIDATION_FAILED = "Address validation failed";
+    public static final String CUSTOMER_VALIDATION_FAILED = "Customer validation failed";
 
     public static final String LOG_BAR = "=======================================================================================" +
             "==========================================";
