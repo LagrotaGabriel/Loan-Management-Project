8c6a0332c119a351aadb591a361a874c39b7b0eb
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-29 08:54:05 -0300

    Revert "Now the installment classes don't make the creation or update of a installment. This will be made betwen other microservice"
    This reverts commit 5b4bbaebfdbd30474265a16cac52d4f3609a8050.

--- backend/loan-api/src/main/java/br/com/loanapi/models/dto/InstallmentDTO.java
+++ backend/loan-api/src/main/java/br/com/loanapi/models/dto/InstallmentDTO.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-
 /** Class that contains all attributes of the object of type InstallmentDTO
  ** @author Gabriel Lagrota
  ** @version 1.0.0
@@ -22,27 +21,14 @@ public class InstallmentDTO {
     @JsonIgnore
     private Long id;
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String maturityDate;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String paymentDate;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Boolean expired;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Integer month;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Double amortization;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Double interest;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Double value;
 
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private LoanDTO loan;
 }
