5e560c334dc3325e04a4185d4e4b743e5772c048
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-31 01:30:12 -0300

    Loan creation successfull Loan creation connection with the Installment manager microservice successfull

--- backend/loan-api/src/main/java/br/com/loanapi/services/LoanService.java
+++ backend/loan-api/src/main/java/br/com/loanapi/services/LoanService.java
@@ -3,22 +3,38 @@ package br.com.loanapi.services;
 import br.com.loanapi.config.ModelMapperConfig;
 import br.com.loanapi.exceptions.InvalidRequestException;
 import br.com.loanapi.exceptions.ObjectNotFoundException;
+import br.com.loanapi.models.dto.InstallmentDTO;
 import br.com.loanapi.models.dto.LoanDTO;
+import br.com.loanapi.models.entities.CustomerEntity;
 import br.com.loanapi.models.entities.LoanEntity;
+import br.com.loanapi.proxys.InstallmentServiceProxy;
+import br.com.loanapi.repositories.CustomerRepository;
 import br.com.loanapi.repositories.LoanRepository;
 import br.com.loanapi.validations.LoanValidation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static br.com.loanapi.utils.StringConstants.*;
+
+@Slf4j
 @Service
 public class LoanService {
 
     @Autowired
     LoanRepository repository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    InstallmentServiceProxy proxy;
 
     @Autowired
     ModelMapperConfig modelMapper;
@@ -28,9 +44,50 @@ public class LoanService {
     LoanValidation validation = new LoanValidation();
 
     public LoanDTO create(LoanDTO loan){
-        if (validation.validateRequest(loan))
+
+        log.info(LOG_BAR);
+        log.info("[STARTING] Starting create method");
+
+        log.info("[PROGRESS] Searching for a customer with the customerId received in JSON...");
+        Optional<CustomerEntity> optionalCustomer = customerRepository.findById(loan.getCustomerJsonId());
+
+        if (validation.validateRequest(loan) && optionalCustomer.isPresent()) {
+
+            CustomerEntity customer = optionalCustomer.get();
+            log.warn("[INFO] Customer found: {} {}", customer.getName(), customer.getLastName());
+
+            log.info("[PROGRESS] Trying to connect into installment calculation micro service...");
+            ResponseEntity<List<InstallmentDTO>> installmentMicroServiceRequest = proxy.calculateInstallments(loan);
+
+            if (installmentMicroServiceRequest.getStatusCode() == HttpStatus.OK) {
+                log.warn("[INFO] Microservice connection successfull");
+                List<InstallmentDTO> loanInstallments = installmentMicroServiceRequest.getBody();
+
+                log.info("[PROGRESS] Setting the calculated installments into the loan...");
+                loan.setInstallments(loanInstallments);
+
+                log.info("[PROGRESS] Setting the debit balance equals than the original loan value");
+                loan.setDebitBalance(loan.getOriginalValue());
+
+                log.info("[PROGRESS] Saving the loan into the customer with id {}", loan.getCustomerJsonId());
+                customer.addLoan(modelMapper.mapper().map(loan, LoanEntity.class));
+
+                log.info("[PROGRESS] Updating the customer in the database...");
+                customerRepository.save(customer);
+            }
+            else {
+                //TODO Create a exception to connection failure with the micro service
+                return null;
+            }
+
+
             return modelMapper.mapper().map(repository.save(modelMapper.mapper().map(loan, LoanEntity.class)), LoanDTO.class);
-        throw new InvalidRequestException("Loan validation failed");
+        }
+        else {
+            log.warn(CUSTOMER_NOT_FOUND_LOG);
+            throw new InvalidRequestException(CUSTOMER_NOT_FOUND);
+        }
+
     }
 
     public List<LoanDTO> findAll() {
