a4135977c2fe5cc47afba8d78cae11eb1b092e71
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-31 01:28:11 -0300

    Connection with Installment manager microservice successfull

---
+++ backend/loan-api/src/main/java/br/com/loanapi/proxys/InstallmentServiceProxy.java
@@ -0,0 +1,18 @@
+package br.com.loanapi.proxys;
+
+import br.com.loanapi.models.dto.InstallmentDTO;
+import br.com.loanapi.models.dto.LoanDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "installment-service", url = "localhost:8091")
+public interface InstallmentServiceProxy {
+
+    @PostMapping(value="installment-service")
+    ResponseEntity<List<InstallmentDTO>> calculateInstallments(@RequestBody LoanDTO loanDTO);
+
+}
