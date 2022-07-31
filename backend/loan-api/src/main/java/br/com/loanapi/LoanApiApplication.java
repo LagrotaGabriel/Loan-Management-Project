90cf09b877044264b8349472187e8c2592d32d73
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-07-31 01:30:34 -0300

    EnableFeignClients annotation created at project main class

--- backend/loan-api/src/main/java/br/com/loanapi/LoanApiApplication.java
+++ backend/loan-api/src/main/java/br/com/loanapi/LoanApiApplication.java
@@ -3,9 +3,11 @@ package br.com.loanapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableFeignClients
 @EntityScan(basePackages = {"br.com.loanapi.models"})
 @EnableJpaRepositories("br.com.loanapi.repositories")
 @ComponentScan("br.com.loanapi.services")
