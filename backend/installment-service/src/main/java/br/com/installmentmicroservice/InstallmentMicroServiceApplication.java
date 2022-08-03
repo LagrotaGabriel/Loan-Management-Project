e0f2b36d8e26951832ab338f0f58b3bf4daefd9a
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-08-03 14:28:47 -0300

    Sprint 2 branch created
    Took 8 seconds

--- backend/installment-service/src/main/java/br/com/installmentmicroservice/InstallmentMicroServiceApplication.java
+++ backend/installment-service/src/main/java/br/com/installmentmicroservice/InstallmentMicroServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
-
 @EntityScan(basePackages = {"br.com.installmentmicroservice.models"})
 @ComponentScan("br.com.installmentmicroservice.services")
 @ComponentScan("br.com.installmentmicroservice.resources")
